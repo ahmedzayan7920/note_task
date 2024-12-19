8c8348b8ca682df3ead8ab4614ff0e7e8f6e73d1
Ahmed Zayan <ahmedabdallazayan@gmail.com>
2024-12-19 14:16:01 +0200

    Add NoteDialogButton widget
    Introduce a new widget `NoteDialogButton` for handling the save functionality in the note dialog.
    
    This widget uses a `BlocBuilder` to listen to the `AddOrUpdateNoteCubit` state and displays a `CustomElevatedButton` that triggers the save action when pressed.
    
    The button's loading state is managed based on the cubit's state.
    
    The save action validates the form and either updates an existing note or adds a new one based on the presence of a note model.

---
+++ lib/features/notes/ui/widgets/note_dialog/note_dialog_button.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../../core/enums/note_color.dart';
+import '../../../../../core/widgets/custom_elevated_button.dart';
+import '../../../logic/add_or_update_note/add_or_update_note_cubit.dart';
+import '../../../logic/add_or_update_note/add_or_update_note_state.dart';
+import '../../../models/note_model.dart';
+
+class NoteDialogButton extends StatelessWidget {
+  const NoteDialogButton({
+    super.key,
+    required this.formKey,
+    this.note,
+    required this.titleController,
+    required this.bodyController,
+    required this.selectedColorIndex,
+  });
+
+  final GlobalKey<State<StatefulWidget>> formKey;
+  final NoteModel? note;
+  final TextEditingController titleController;
+  final TextEditingController bodyController;
+  final int selectedColorIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<AddOrUpdateNoteCubit, AddOrUpdateNoteState>(
+      builder: (context, state) {
+        return CustomElevatedButton(
+          text: 'Save',
+          isLoading: state is AddOrUpdateNoteLoadingState,
+          onPressed: () => _save(context: context),
+        );
+      },
+    );
+  }
+
+  _save({required BuildContext context}) {
+    if ((formKey.currentState as FormState).validate()) {
+      final newNote = NoteModel(
+        id: note?.id ?? '',
+        title: titleController.text,
+        body: bodyController.text,
+        color: NoteColor.values[selectedColorIndex],
+        createdAt: note?.createdAt ?? DateTime.now(),
+      );
+      note != null
+          ? context.read<AddOrUpdateNoteCubit>().updateNote(note: newNote)
+          : context.read<AddOrUpdateNoteCubit>().addNote(note: newNote);
+    }
+  }
+}
