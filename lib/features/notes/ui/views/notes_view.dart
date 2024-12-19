f75aef108010b0eb282ecbf5681862f6caad5588
Ahmed Zayan <ahmedabdallazayan@gmail.com>
2024-12-19 14:26:10 +0200

    Integrate Bloc and Firestore for notes management
    Updated NotesView to use BlocProvider and NotesCubit for state management.
    
    Added Firestore integration through NoteRepository to fetch and display notes.
    
    Included a NotesViewBuilder widget to render the list of notes and added spacing between the search section and notes list.

--- lib/features/notes/ui/views/notes_view.dart
+++ lib/features/notes/ui/views/notes_view.dart
@@ -1,5 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_task/features/notes/repos/note_repository.dart';
+
+import '../../logic/notes/notes_cubit.dart';
 import '../widgets/note_dialog/note_dialog.dart';
+import '../widgets/notes/notes_view_builder.dart';
 import '../widgets/search/search_section.dart';
 
 class NotesView extends StatelessWidget {
@@ -7,28 +13,39 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[300],
-      body: const SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            children: [
-              SearchSection(),
-            ],
+    return BlocProvider(
+      create: (context) => NotesCubit(
+        noteRepository: NoteRepository(
+          firestore: FirebaseFirestore.instance,
+        ),
+      )..fetchNotes(),
+      child: Scaffold(
+        backgroundColor: Colors.grey[300],
+        body: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              children: [
+                SearchSection(),
+                SizedBox(height: 16),
+                Expanded(
+                  child: NotesViewBuilder(),
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return const NoteDialog();
-            },
-          );
-        },
-        child: const Icon(Icons.add),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return const NoteDialog();
+              },
+            );
+          },
+          child: const Icon(Icons.add),
+        ),
       ),
     );
   }
